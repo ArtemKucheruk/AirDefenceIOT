7c4c561b5737ade73733709212e2d42b3063ffd1
Danya <r1028011@student.thomasmore.be>
2025-05-09 14:33:36 +0200

    motor code 2'

--- stepper motor/motor.py
+++ stepper motor/motor.py
@@ -1,36 +1,46 @@
 import wiringpi
 import time
 
-# WiringPi pins
-PUL_PIN = 2    # w2 = GPIO12 = Physical Pin 7
-DIR_PIN = 3    # w3 = GPIO13 = Physical Pin 8
-ENA_PIN = 11   # w11 = GPIO19 = Physical Pin 35
+# Motor 1 pins (WiringPi numbers)
+PUL1 = 2     # w2 = GPIO12
+DIR1 = 3     # w3 = GPIO13
+ENA1 = 11    # w11 = GPIO19
+
+# Motor 2 pins (WiringPi numbers)
+PUL2 = 4     # w4 = GPIO16
+DIR2 = 5     # w5 = GPIO1
+ENA2 = 6     # w6 = GPIO4
 
 # Setup
-wiringpi.wiringPiSetup()  # Use WiringPi pin numbering
-wiringpi.pinMode(PUL_PIN, 1)  # Set as OUTPUT
-wiringpi.pinMode(DIR_PIN, 1)
-wiringpi.pinMode(ENA_PIN, 1)
+wiringpi.wiringPiSetup()
 
-# Enable motor (ENA+ LOW = enable)
-wiringpi.digitalWrite(ENA_PIN, 0)
+# Set all pins as output
+for pin in [PUL1, DIR1, ENA1, PUL2, DIR2, ENA2]:
+    wiringpi.pinMode(pin, 1)
 
-def run_motor(delay, duration, direction):
-    wiringpi.digitalWrite(DIR_PIN, direction)  # 1 = CW, 0 = CCW
+# Enable both motors
+wiringpi.digitalWrite(ENA1, 0)
+wiringpi.digitalWrite(ENA2, 0)
+
+def run_motor(pul_pin, dir_pin, delay, duration, direction):
+    wiringpi.digitalWrite(dir_pin, direction)
     steps = int(duration / (delay * 2))
-    print(f"Running {'CW' if direction else 'CCW'} for {duration}s at delay {delay}s")
-    for i in range(steps):
-        wiringpi.digitalWrite(PUL_PIN, 1)
+    for _ in range(steps):
+        wiringpi.digitalWrite(pul_pin, 1)
         time.sleep(delay)
-        wiringpi.digitalWrite(PUL_PIN, 0)
+        wiringpi.digitalWrite(pul_pin, 0)
         time.sleep(delay)
 
 try:
-    run_motor(delay=0.002, duration=5, direction=1)   # Slow CW
-    run_motor(delay=0.001, duration=5, direction=1)   # Faster CW
-    run_motor(delay=0.0005, duration=5, direction=1)  # Fastest CW
-    run_motor(delay=0.0005, duration=5, direction=0)  # Fastest CCW
+    print("Motor 1 CW, Motor 2 CCW (slow)")
+    run_motor(PUL1, DIR1, 0.002, 5, 1)
+    run_motor(PUL2, DIR2, 0.002, 5, 0)
+
+    print("Motor 1 CW, Motor 2 CCW (fast)")
+    run_motor(PUL1, DIR1, 0.0005, 5, 1)
+    run_motor(PUL2, DIR2, 0.0005, 5, 0)
 
 finally:
-    wiringpi.digitalWrite(ENA_PIN, 1)  # Disable motor
-    print("Motor test finished.")
+    wiringpi.digitalWrite(ENA1, 1)
+    wiringpi.digitalWrite(ENA2, 1)
+    print("Motors disabled.")
